1b9e233bdf1d838f73a8b09eccb7022decc53f43
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2025-01-01 22:47:16 +0900

    Setup

---
+++ Assets/character.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int a = 0;
+    void Start()
+    {
+        a = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
